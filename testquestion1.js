dac5cc733567fb40d1f196d2a33827d158d5da19
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-09-06 07:51:43 +0000

    Sep 6, 2026, 7:51 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,18 @@
+let balls;
 
+function setup(){
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(220);
+    if (mouse.presses()){
+        spawnBall();
+    }
+}
+
+function spawnBall(){
+    balls = new Sprite(mouseX,mouseY,30);
+    balls = 'dynamic';
+    world.gravity.y = 5;
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,21 @@
+let balls,floor;
 
+function setup(){
+    createCanvas(600, 400);
+    floor = new Sprite(0,400,1200,100);
+    floor.collider = 'static';
+}
+
+function draw() {
+    background(220);
+    if (mouse.presses()){
+        spawnBall();
+    }
+}
+
+function spawnBall(){
+    balls = new Sprite(mouseX,mouseY,30);
+    balls.collider = 'dynamic';
+    world.gravity.y = 5;
+    balls.bounciness = 1;
+}
